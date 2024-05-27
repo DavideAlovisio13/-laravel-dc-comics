19df6debc9bf9b834dcf9c634495de62234d4df0
DavideAlovisio13 <alovisiod@gmail.com>
2024-05-27 16:12:56 +0200

    populate db

--- database/migrations/2024_05_27_134740_create_comics_table.php
+++ database/migrations/2024_05_27_134740_create_comics_table.php
@@ -15,6 +15,13 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 250);
+            $table->text('description')->nullable();
+            $table->string('thumb', 250)->nullable();
+            $table->decimal('price', 5, 2);
+            $table->string('series', 250);
+            $table->date('sale_date');
+            $table->string('type', 250);
             $table->timestamps();
         });
     }
