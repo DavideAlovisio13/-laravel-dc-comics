a9a320e87da011dc5365ba8a9cc775aa404d55ed
DavideAlovisio13 <alovisiod@gmail.com>
2024-05-29 18:54:51 +0200

    (show.blade) create new layout (edit, create.blade) add select option

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,24 +1,24 @@
 @extends('layouts.app')
 @section('title', $comic->title)
 @section('content')
-<section class="container bg-white mt-4">
-    <h1 class="text-center">{{$comic->title}}</h1>
-    <div class="row">
-        <div class="col-12 col-md-4">
-            <img src="{{$comic->thumb}}" class="img-fluid" alt="{{$comic->title}}">
+    <section class="py-5 bg-footer text-white">
+        <div class="container px-4 px-lg-5 my-5">
+            <div class="row gx-4 gx-lg-5 align-items-center">
+                <div class="col-md-6">
+                    <img class="card-img-top mb-5 mb-md-0"
+                        src="{{ $comic->thumb }}" alt="{{ $comic->title }}" />
+                </div>
+                <div class="col-md-6">
+                    <h1 class="display-5 fw-bolder">{{ $comic->title }}</h1>
+                    <div class="fs-5 mb-5">
+                        <span>$ {{ $comic->price }}</span>
+                    </div>
+                    <p class="lead">{{ $comic->description }}</p>
+                    <p class="lead"><strong>Serie:</strong> {{ $comic->series}}</p>
+                    <p class="lead"><strong>Data di vendita:</strong> {{ $comic->sale_date }}</p>
+                    <p class="lead"><strong>Tipo:</strong> {{ $comic->type }}</p>
+                </div>
+            </div>
         </div>
-        <div class="col-12 col-md-8">
-            <p>{!!$comic->description!!}</p>
-            <div>
-                Tipo: {{$comic->type}}
-            </div>
-            <div>
-                Serie: {{$comic->series}}
-            </div>
-            <div>
-                Data di vendita: {{$comic->sale_date}}
-            </div>
-        </div>
-    </div>
-</section>
+    </section>
 @endsection
